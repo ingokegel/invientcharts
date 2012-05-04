d6461d1aeba7ef5f59131dc08a6143c213cb1b27
Ingo Kegel <ingo.kegel@ej-technologies.com>
2012-05-04 21:50:54 +0200

    Updated demo with new dependencies and recompiled widget set

--- demo/com/invient/vaadin/InvientChartsDemoAppServlet.java
+++ demo/com/invient/vaadin/InvientChartsDemoAppServlet.java
@@ -34,7 +34,7 @@ public class InvientChartsDemoAppServlet extends ApplicationServlet {
             HttpServletRequest request) throws ServletException, IOException {
         page.write("<script type=\"text/javascript\">\n");
         page.write("//<![CDATA[\n");
-        page.write("document.write(\"<script language='javascript' src='./jquery/jquery-1.4.4.min.js'><\\/script>\");\n");
+        page.write("document.write(\"<script language='javascript' src='./jquery/jquery-1.7.2.min.js'><\\/script>\");\n");
         page.write("document.write(\"<script language='javascript' src='./js/highcharts.js'><\\/script>\");\n");
         page.write("document.write(\"<script language='javascript' src='./js/modules/exporting.js'><\\/script>\");\n");
         page.write("//]]>\n</script>\n");
